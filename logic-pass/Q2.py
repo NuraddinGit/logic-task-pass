22990e6687d3cc23e149a65b897b52eefb673f34
Nuraddin <93943326+NuraddinGit@users.noreply.github.com>
2022-08-21 00:21:40 +0300

    Create Q2.py

---
+++ logic-pass/Q2.py
@@ -0,0 +1,10 @@
+# Q2/Write a program to find all prime numbers up to a given range of numbers?
+
+min = int(input("Enter the min : "))
+max = int(input("Enter the max : "))
+for n in range(min, max + 1):
+    for i in range(2, n):
+        if (n % i) == 0:
+            break
+    else:
+        print(n)
